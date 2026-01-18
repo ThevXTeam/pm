6cac996672af62bdc5d6c1d1cf68386efb34fdf1
MxPerrot <44461157+MxPerrot@users.noreply.github.com>
2026-01-18 23:37:56 +0100

    debug

--- alias.lua
+++ alias.lua
@@ -1,4 +1,6 @@
 -- create a `pm` alias that delegates to `/vx/pm/init.lua` when available
 if fs.exists("/vx/pm/init.lua") then
   pcall(function() shell.setAlias("pm", "/vx/pm/init.lua") end)
+else
+  print("No pm script found at /vx/pm/init.lua")
 end
